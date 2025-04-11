d73ffdb4a233f9c4d00b809a019c69f37bd151fe
xZeyshow <190229939+xZeyshow@users.noreply.github.com>
2025-04-11 19:50:36 +0200

    v3.1

--- pages/contact.php
+++ pages/contact.php
@@ -3,10 +3,12 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=², initial-scale=1.0">
-    <title>Contact</title>
+    <title>Merch-Unity</title>
     <script src="https://cdn.tailwindcss.com"></script> <!-- FrameWork Tailwind -->
     <link href="../css_pages/style.css" rel="stylesheet" />
      <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
+     <link href="../css_pages/login.css" rel="stylesheet" />
+     <link rel="icon" href="../images/index_page/Image2-modified.png" type="image/png">
 </head>
 <body>
 
@@ -71,13 +73,27 @@
 
 <br><br>
     
-  
+<?php
+  if (isset($_POST['submit'])) {
+    echo '
+    <div class="container text-center">
+      <div class="success-msg">
+    <i class="fa fa-check"></i>
+    Le message a bien été envoyé...
+  </div>
+    </div>
+    
+    ';
+  }
+ 
+?>
+
 <!-- Formulaire -->
   <div class="mx-auto max-w-2xl text-center">
     <h2 class="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">Nous contacter</h2>
 
   </div>
-  <form action="#" method="post" class="mx-auto mt-8 max-w-xl sm:mt-20">
+  <form action="contact.php" method="POST" class="mx-auto mt-8 max-w-xl sm:mt-20">
     <div class="grid grid-cols-1 gap-x-8 gap-y-6 sm:grid-cols-2">
       <div>
         <label for="first-name" class="block text-sm font-semibold leading-6 text-gray-900">Prénom</label>
@@ -128,7 +144,7 @@
       </div>
     </div>
     <div class="mt-10">
-      <button type="submit" class="block w-full rounded-md bg-indigo-600 px-3.5 py-2.5 text-center text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Envoyer</button>
+      <button type="submit" name="submit" class="block w-full rounded-md bg-indigo-600 px-3.5 py-2.5 text-center text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Envoyer</button>
     </div>
   </form>
 </div>
